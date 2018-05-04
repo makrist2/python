dddc46d639f4e426e7ed4510c271b2287f4faaf6
makrist2 <namerki@gmail.com>
2018-05-04 21:52:38 +0300

    103-104

---
+++ hw_1.py
@@ -0,0 +1,23 @@
+from math import *
+
+a = 10
+b = 6
+c = 60
+
+test1 = a + b * ( c / 2 )
+print(test1)
+
+test2 = ( a*a + b*b) % 2
+print(test2)
+
+test3 = (a + b) / 12 * c % 4 + b
+print(test3)
+
+test4 = (a - b * c) / (a + b) % c
+print(test4)
+
+test5 = abs(a - b) / (a + b)**3 - cos(c)
+print(test5)
+
+test6 = (log(1 + c) / -b)**4 + abs(a)
+print(test6)
