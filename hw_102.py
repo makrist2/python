2fb4ff89411b72f4e0fd3e843480ec17f25928df
makrist2 <namerki@gmail.com>
2018-05-05 23:22:20 +0300

    hw extra 102

--- hw_102.py
+++ hw_102.py
@@ -1,7 +1,7 @@
 import re
 import os
 
-filename = os.path.basename('C:\\Users\\makri\\Desktop\\folder\\SomeDocument1.txt')
+filename = os.path.basename('C:\\Users\\makri\\Desktop\\folder\\SomeDocument1.docx')
 filename1 = os.path.splitext(filename)[0]
 
 i_hope_its_right = re.sub(r'SomeDocument1', "<A HREF=\"" + filename1 + '.docx >' + filename1 + '</A>', filename1)
