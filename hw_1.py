3bdb0f148137b3d50cf13e660bb9d81020fcd2cc
makrist2 <namerki@gmail.com>
2018-05-05 12:26:07 +0300

    hw 1

--- hw_1.py
+++ hw_1.py
@@ -1,13 +1,13 @@
-from math import *
+import math
 
 a = 10
 b = 6
 c = 60
 
-test1 = a + b * ( c / 2 )
+test1 = a + b * (c / 2)
 print(test1)
 
-test2 = ( a*a + b*b) % 2
+test2 = (a*a + b*b) % 2
 print(test2)
 
 test3 = (a + b) / 12 * c % 4 + b
@@ -16,8 +16,8 @@ print(test3)
 test4 = (a - b * c) / (a + b) % c
 print(test4)
 
-test5 = abs(a - b) / (a + b)**3 - cos(c)
+test5 = abs(a - b) / (a + b)**3 - math.cos(c)
 print(test5)
 
-test6 = (log(1 + c) / -b)**4 + abs(a)
+test6 = (math.log(1 + c) / -b)**4 + abs(a)
 print(test6)
