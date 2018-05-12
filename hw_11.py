cbb804318a2e18c3bc7f313673012ce40c4b50ec
makrist2 <38978002+makrist2@users.noreply.github.com>
2018-05-12 20:44:07 +0300

    Update hw_11.py

--- hw_11.py
+++ hw_11.py
@@ -1,8 +1,8 @@
 import math
 
+
 def degrees2radians(degrees):
-    convert = math.radians(degrees)
-    cos = math.cos(convert)
-    print('Value = %f' % cos)  # я правильно понял задание?
+    cos = math.cos(degrees)
+    print('Value = %f' % cos)  
 
 degrees2radians(60)
