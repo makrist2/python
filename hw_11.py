afbac778c7dae602f8ebe650331a44572160096a
makrist2 <namerki@gmail.com>
2018-05-13 20:53:59 +0300

    Написать функцию, которая будет переводить градусы в радианы.

--- hw_11.py
+++ hw_11.py
@@ -1,7 +1,10 @@
 import math
 
-def degrees2radians(degrees):
-    cos = math.cos(degrees)
-    print('Value = %f' % cos)  # я правильно понял задание?
 
-degrees2radians(60)
+def degrees2radians(degrees):
+    return degrees*180/math.pi
+
+
+print('Cosine of 45: %.2f' % math.cos(degrees2radians(45)))
+print('Cosine of 40: %.2f' % math.cos(degrees2radians(40)))
+print('Cosine of 60: %.2f' % math.cos(degrees2radians(60)))
