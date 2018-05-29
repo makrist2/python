4020a98b5d34227a5a142528b9da14b5d687b041
makrist2 <namerki@gmail.com>
2018-05-29 12:59:13 +0300

    hw 23

--- hw_23.py
+++ hw_23.py
@@ -1,9 +1,11 @@
 def chess_reward():
-    for i in range(0, 64):
-        for x in range(i):
-            i = 2 ** x - 1
-            if i > 1000000:
-                return i, x
+    i = 1
+    c = 1
+    while c <= 64:
+        i *= 2
+        c += 1
+        if i > 1000000:
+            return c, i
 
 
 print(chess_reward())
