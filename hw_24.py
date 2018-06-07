c12f15729c0c9b4a5a08957a241a3c5f8422d6ff
makrist2 <namerki@gmail.com>
2018-06-07 22:06:13 +0300

    hw 24 Провтыкал букву N в описании группы

--- hw_24.py
+++ hw_24.py
@@ -1,7 +1,7 @@
 def group_by_surname(list_of_enrollees):
     first = 'ABCDEFGHI'
     f = 0
-    second = 'JKLMOP'
+    second = 'JKLMNOP'
     s = 0
     third = 'QRST'
     t = 0
@@ -22,6 +22,6 @@ def group_by_surname(list_of_enrollees):
         return f, s, t, fo
 
 
-lst = ['Will Smith', 'Jay Z', 'Leonardo DiCaprio', 'George Orwell']
+lst = ['Johnny Depp', 'Al Pacino', 'Kevin Spacey', 'Denzel Washington', 'Russell Crowe', 'Brad Pitt', 'Angelina Jolie', 'Leonardo DiCaprio', 'Tom Cruise', 'John Travolta', 'Arnold Schwarzenegger', 'Sylvester Stallone', 'Kate Winslet', 'Christian Bale', 'Morgan Freeman', 'Keanu Reeves', 'Hugh Jackman', 'Edward Norton', 'Bruce Willis', 'Tom Hanks', 'Charlize Theron', 'Will Smith', 'Sean Connery', 'Keira Knightley', 'Vin Diesel', 'Matt Damon', 'Richard Gere', 'Catherine Zeta-Jones', 'Clive Owen', 'Mel Gibson', 'George Clooney', 'Jack Nicholson', 'Scarlett Johansson', 'Tom Hardy', 'Samuel Jackson', 'Sandra Bullock', 'Meg Ryan', 'Nicole Kidman', 'Simon Baker', 'Cameron Diaz', 'Anthony Hopkins']
 print(group_by_surname(lst))
 
