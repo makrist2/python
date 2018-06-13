6ced04f75c7fd7b809291a84d8dce5373b66c56d
makrist2 <namerki@gmail.com>
2018-06-13 12:11:24 +0300

    test 7

---
+++ test_7.py
@@ -0,0 +1,11 @@
+def fibonacci(n):
+    fib1, fib2 = 0, 1
+    for i in range(n):
+        fib1, fib2 = fib2, fib1 + fib2
+        yield fib1
+
+
+for _ in fibonacci(10):
+    print(_, end=' ')
+
+print('\n', sum(fibonacci(10)))
