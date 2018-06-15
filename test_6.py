71b90631e55deea9239fa41687e625c1c8277b34
makrist2 <namerki@gmail.com>
2018-06-15 11:02:25 +0300

    test 6

--- test_6.py
+++ test_6.py
@@ -1,9 +1,11 @@
 def is_isogram(word):
     true_word = word.lower()
-    for char in true_word:
-        if true_word.count(char) > 1:
-            return False
-    return True
+    if len(word.replace(' ', '')) == len(set(true_word.replace(' ', ''))):
+        return True
+    else:
+        return False
 
 
 print(is_isogram('ms acquired g?th_b'))
+print(is_isogram('wheel'))
+print(is_isogram('machine'))
