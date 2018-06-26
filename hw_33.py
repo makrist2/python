1b6b2bedb9f5cedf12c0c3e517d1a43ce92823fa
makrist2 <namerki@gmail.com>
2018-06-26 22:42:19 +0300

    tried to use pandas

--- hw_33.py
+++ hw_33.py
@@ -1,70 +1,10 @@
-import csv
-from pprint import pprint as pp
+import pandas
 
+titanic = pandas.read_csv('titanic.csv')
+#print(titanic)
 
-def get_data_from_csv(filename):
-    f = open(filename, "r")
-    list_dicts = [row for row in csv.DictReader(f)]
-    f.close()
-    return list_dicts
+sex_stats = titanic.groupby(['Sex', 'Survived'])['PassengerId'].count()
+pclass_stats = titanic.groupby(['Pclass', 'Survived'])['PassengerId'].count()
 
-
-def survived_percentage(passengers, field):
-    if field == 'Sex':
-        surv_sex_info = {'male_survived': 0,
-                         'female_survived': 0,
-                         'total_male': 0,
-                         'total_female': 0}
-        for fieldd in passengers:
-            if fieldd['Sex'] == 'male':
-                if fieldd['Survived'] == '1':
-                    surv_sex_info['male_survived'] += 1
-                surv_sex_info['total_male'] += 1
-            if fieldd['Sex'] == 'female':
-                if fieldd['Survived'] == '1':
-                    surv_sex_info['female_survived'] += 1
-                surv_sex_info['total_female'] += 1
-        survived = {
-            field: {'Male': '{:.2%}'.format(surv_sex_info['male_survived'] / surv_sex_info['total_male']),
-                    'Female': '{:.2%}'.format(surv_sex_info['female_survived'] / surv_sex_info['total_female'])}
-        }
-        return survived
-
-    elif field == 'Pclass':
-        class_surv_info = {
-            'first_class_survived': 0,
-            'second_class_survived': 0,
-            'third_class_survived': 0,
-            'total_first_class': 0,
-            'total_second_class': 0,
-            'total_third_class': 0}
-
-        for fieldd in passengers:
-            if fieldd['Pclass'] == '1':
-                if fieldd['Survived'] == '1':
-                    class_surv_info['first_class_survived'] += 1
-                class_surv_info['total_first_class'] += 1
-            if fieldd['Pclass'] == '2':
-                if fieldd['Survived'] == '1':
-                    class_surv_info['second_class_survived'] += 1
-                class_surv_info['total_second_class'] += 1
-            if fieldd['Pclass'] == '3':
-                if fieldd['Survived'] == '1':
-                    class_surv_info['third_class_survived'] += 1
-                class_surv_info['total_third_class'] += 1
-
-        survived = {
-            field: {'First class survived percentage': '{:.2%}'.format(
-                class_surv_info['first_class_survived'] / class_surv_info['total_first_class']),
-                    'Second class survived percentage': '{:.2%}'.format(
-                        class_surv_info['second_class_survived'] / class_surv_info['total_second_class']),
-                    'Third class survived percentage': '{:.2%}'.format(
-                        class_surv_info['third_class_survived'] / class_surv_info['total_third_class'])}
-        }
-        return survived
-
-
-pp(survived_percentage(get_data_from_csv('titanic.csv'), 'Sex'))
-pp(survived_percentage(get_data_from_csv('titanic.csv'), 'Pclass'))
-
-# pp(get_data_from_csv('titanic.csv'))
+print(sex_stats)
+print(pclass_stats)
