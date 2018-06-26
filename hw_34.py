019a6a1cc8579acc78ad58ce9dc8bf8149a710b6
makrist2 <namerki@gmail.com>
2018-06-26 21:35:45 +0300

    34 fix

--- hw_34.py
+++ hw_34.py
@@ -1,24 +1,23 @@
 class Godzilla:
+    STOMACH_LIM = 0.9
 
-    STOMACH_VOL = 1000
-
-    def __init__(self):
+    def __init__(self, stomach_vol):
         self.stomach = 0
+        self.stomach_vol = stomach_vol
 
     def eat(self, person_vol):
-        if self.stomach + person_vol >= 0.9 * self.STOMACH_VOL:
+        if self.stomach + person_vol >= self.STOMACH_LIM * self.stomach_vol:
             print('GODZILLA FULL!! THANK YOU HUMAN BEINGS!')
         else:
             self.stomach += person_vol
             print('GIMME MORE FOOD ')
-        if person_vol > self.STOMACH_VOL:
-            print('THIS HUMAN SO BIG THAT GODZILLA CAN\'T PLACE IT IN ITS MOUTH!!11!')
+        if person_vol > self.stomach_vol:
+            print('THIS HUMAN SO BIG GODZILLA CAN\'T PLACE IT IN ITS MOUTH!!11!')
 
 
-best_godzilla = Godzilla()
+best_godzilla = Godzilla(1000)
 best_godzilla.eat(300)
 best_godzilla.eat(300)
 best_godzilla.eat(300)
 best_godzilla.eat(300)
-# best_godzilla.eat(10000000)
-
+best_godzilla.eat(100000)
